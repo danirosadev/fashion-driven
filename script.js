40e9831e6c9e201c46d892aedc3fdec142f38808
Dani Rosa <danirosa.dev@gmail.com>
2022-07-12 18:42:28 -0300

    comunicação com servidor com erro

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-let nome;
+let owner;
 let modeloSelecionado, golaSelecionada, tecidoSelecionado;
-let imagemValida;
+let imagem;
+let camisa;
+let novaCamisa = [];
+let tipoModelo, tipoGola, tipoTecido;
+let author;
+
 
 function perguntarNome(){
-    nome = prompt('Olá! Como você gostaria de ser chamadx?')
-    return nome;
+    author = prompt('Olá! Como você gostaria de ser chamadx?');
+}    
+
+function mostrarRecentes(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts');
+    promise.then(pegaRecentes);
 }
 
-function selecionarModelo(elemento){
+function pegaRecentes(resposta){
+    camisa = resposta.data;
+    renderizaRecentes();
+}
+
+function renderizaRecentes(){
+
+    let criadosRecentemente = document.querySelector('.imagens');
+    
+    for (let i = 0; i < camisa.length; i++){
+        let template = `
+        <div>
+            <img src="${camisa[i].image}" />
+            <p><strong>Criador: </strong>${camisa[i].owner}</p>
+        </div>
+    `
+        criadosRecentemente.innerHTML += template
+    }
+}
+
+function selecionarModelo(tipo, elemento){
+    tipoModelo = tipo;
+
     modeloSelecionado = elemento.innerHTML;
 
     let modelo = document.querySelector('.modelo .selecionado');
@@ -20,7 +51,9 @@ function selecionarModelo(elemento){
     enviarPedido();
 }
 
-function selecionarGola(elemento){
+function selecionarGola(tipo, elemento){
+    tipoGola = tipo;
+
     golaSelecionada = elemento.innerHTML;
 
     let gola = document.querySelector('.gola .selecionado');
@@ -33,7 +66,9 @@ function selecionarGola(elemento){
     enviarPedido();
 }
 
-function selecionarTecido(elemento){
+function selecionarTecido(tipo, elemento){
+    tipoTecido = tipo;
+
     tecidoSelecionado = elemento.innerHTML;
 
     let tecido = document.querySelector('.tecido .selecionado');
@@ -46,9 +81,9 @@ function selecionarTecido(elemento){
     enviarPedido();
 }
 
-function isURL() {
+function isURL(imagem) {
     regularExpression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
-    return regularExpression.test();
+    return regularExpression.test(imagem);
 }
 
 function enviarPedido(){
@@ -62,7 +97,43 @@ function enviarPedido(){
 }
 
 function confirmarPedido(){
-    alert(`Você encomendou um ${modeloSelecionado} com ${golaSelecionada} de ${tecidoSelecionado}`)
+    imagem = document.querySelector('input').value;
+    
+    modeloSelecionado = document.querySelector('.selecionado .nome-modelo').innerHTML;
+    golaSelecionada = document.querySelector('.selecionado .nome-gola').innerHTML;
+    tecidoSelecionado = document.querySelector('.selecionado .nome-tecido').innerHTML;
+
+    if (isURL(imagem) != true){
+        alert('Por favor, preencha todos os campos corretamente.')
+    } else {
+        alert(`Você encomendou um ${modeloSelecionado} com ${golaSelecionada} de ${tecidoSelecionado}`)
+    }
+
+    enviarCamisa();
 }
+
+function enviarCamisa(){
+
+    novaCamisa = {
+        model: tipoModelo,
+        neck: tipoGola,
+        material: tipoTecido,
+        owner: author,
+        image: imagem
+    }
+
+    const promise = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', novaCamisa);
+    promise.then(encomendaConfirmada);
+    promise.catch(erroNaEncomenda);    
+}
+
+function encomendaConfirmada(){
+    alert('tudo certo')
+}
+
+function erroNaEncomenda(){
+    alert('Ops, não conseguimos processar sua encomenda')
+}
+
 
 perguntarNome();
